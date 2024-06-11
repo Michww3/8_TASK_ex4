c7f69701785cd0348a4b8cc2996b2d8b8978d55e
User <User@W11PC>
2024-06-11 14:18:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Serialization;
+
+public class User
+{
+    //[XmlAttribute]
+    public string name { get; set; }
+    //[XmlAttribute]
+    public int age { get; set; }
+
+    public User() { }
+
+    public User(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(User));
+
+        User user;
+        using (StreamReader reader = new StreamReader("C:\\Users\\User\\Desktop\\User.xml"))
+        {
+            user = (User)serializer.Deserialize(reader);
+        }
+
+        Console.WriteLine($"Имя: {user.name}, Возраст: {user.age}");
+    }
+}
